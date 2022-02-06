5d8a76ee26a8c8e0c76177582da0e98f2740bb53
a.padolina@sasukefinlab.com <a.padolina@sasukefinlab.com>
2022-02-06 14:01:09 +0900

    finish api only nalang

--- src/Modal/Modalver.js
+++ src/Modal/Modalver.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import Modal from "react-responsive-modal";
 import 'react-responsive-modal/styles.css';
 import Iframe from 'react-iframe';
+import ReactPlayer from 'react-player'
 
 function Modalver() {
   const [open, setOpen] = useState(false);
@@ -12,10 +13,10 @@ function Modalver() {
   return (
     <div>
         <div>
-          <div class="row">
+          <div className="row">
               <h2>NETFLIX ORIGINALS</h2>
-              <div class="row__posters">
-                  <img onClick={onOpenModal} class="row__poster row__posterLarge" src="https://www.billboard-japan.com/common/sys/img/news/00000092/92922/image.jpg" alt="" />
+              <div className="row__posters">
+                  <img onClick={onOpenModal} className="row__poster row__posterLarge" src="https://www.billboard-japan.com/common/sys/img/news/00000092/92922/image.jpg" alt="" />
               </div>
           </div>
           <Modal 
@@ -45,12 +46,18 @@ function Modalver() {
             }
           }}
           >
-            <Iframe src="https://drive.google.com/file/d/1xtp5FJnZvQI-xb2TGxvQQsVjKGkUgDXZ/preview" 
+            {/* <Iframe src="https://videos-netflix-clone.s3.ap-northeast-1.amazonaws.com/video01.mp4" 
             width="100%" 
             height="100%" 
             allow="autoplay"
             allowFullScreen={true}
-            ></Iframe>
+            ></Iframe> */}
+            <ReactPlayer
+              controls
+              width="100%"
+              height="100%"
+              url="https://videos-netflix-clone.s3.ap-northeast-1.amazonaws.com/video01.mp4"
+            />
           </Modal>
         </div>
     </div>
