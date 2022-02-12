d0f1ec3bda90beba58799da6508fa71a14ab9e53
a.padolina@sasukefinlab.com <a.padolina@sasukefinlab.com>
2022-02-12 21:43:07 +0900

    pushing data

--- src/Modal/Modalver.js
+++ src/Modal/Modalver.js
@@ -1,97 +1,67 @@
-import { useEffect, useState } from "react";
+import React,{useState} from 'react'
 import Modal from "react-responsive-modal";
-import "react-responsive-modal/styles.css";
-import ReactPlayer from "react-player";
-import firebaseApp from "../firebase/database";
+import 'react-responsive-modal/styles.css';
+import Iframe from 'react-iframe';
+import ReactPlayer from 'react-player'
 
 function Modalver() {
-	const [open, setOpen] = useState(false);
-	const [error, setError] = useState(null);
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [items, setItems] = useState([]);
+  const [open, setOpen] = useState(false);
 
-	const onOpenModal = () => setOpen(true);
-	const onCloseModal = () => setOpen(false);
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
 
-	useEffect(() => {
-		fetch(
-			"https://movie-clone-a5867-default-rtdb.firebaseio.com/movies/actionMovies.json"
-		)
-			.then((res) => res.json())
-			.then(
-				(result) => {
-					setIsLoaded(true);
-					setItems(result);
-				},
-				// Note: it's important to handle errors here
-				// instead of a catch() block so that we don't swallow
-				// exceptions from actual bugs in components.
-				(error) => {
-					setIsLoaded(true);
-					setError(error);
-				}
-			);
-	}, []);
-
-	if (error) {
-		return <div>Error: {error.message}</div>;
-	} else if (!isLoaded) {
-		return <div>Loading...</div>;
-	} else {
-		return (
-			<div>
-				<div>
-					<div className="row">
-						<h2>NETFLIX ORIGINALS</h2>
-					</div>
-					<div className="row__posters">
-						{items.map((movies_array) => (
-							<img
-								className="row__poster row__posterLarge"
-								src={`${movies_array.thumbnail}`}
-								onClick={onOpenModal}
-							/>
-						))}
-					</div>
-
-					<Modal
-						open={open}
-						onClose={onCloseModal}
-						center
-						closeOnEsc
-						showCloseIcon
-						blockScroll="true"
-						styles={{
-							modal: {
-								maxWidth: "102%",
-								maxHeight: "102%",
-								height: "100%",
-								width: "100%",
-								padding: "0px",
-								left: "-22px",
-								top: "-18px",
-							},
-							overlay: {
-								background: "rgba(0, 0, 0, 0.5)",
-							},
-							closeButton: {
-								background: "white",
-								top: "20px",
-								right: "60px",
-							},
-						}}
-					>
-						<ReactPlayer
-							controls
-							width="100%"
-							height="100%"
-							url="https://videos-netflix-clone.s3.ap-northeast-1.amazonaws.com/Safehouse+2012+Dvdrip+x264.mp4"
-						/>
-					</Modal>
-				</div>
-			</div>
-		);
-	}
+  return (
+    <div>
+        <div>
+          <div className="row">
+              <h2>NETFLIX ORIGINALS</h2>
+              <div className="row__posters">
+                  <img onClick={onOpenModal} className="row__poster row__posterLarge" src="https://www.billboard-japan.com/common/sys/img/news/00000092/92922/image.jpg" alt="" />
+              </div>
+          </div>
+          <Modal 
+          open={open} 
+          onClose={onCloseModal} 
+          center
+          closeOnEsc
+          showCloseIcon
+          blockScroll="true"
+          styles={{
+            modal: {
+              maxWidth: "102%",
+              maxHeight: "102%",
+              height:"100%",
+              width: "100%",
+              padding: "0px",
+              left:"-22px",
+              top:"-18px"
+            },
+            overlay: {
+              background: "rgba(0, 0, 0, 0.5)"
+            },
+            closeButton: {
+              background: "white",
+              top:"20px",
+              right:"60px"
+            }
+          }}
+          >
+            {/* <Iframe src="https://videos-netflix-clone.s3.ap-northeast-1.amazonaws.com/video01.mp4" 
+            width="100%" 
+            height="100%" 
+            allow="autoplay"
+            allowFullScreen={true}
+            ></Iframe> */}
+            <ReactPlayer
+              controls
+              width="100%"
+              height="100%"
+              url="https://videos-netflix-clone.s3.ap-northeast-1.amazonaws.com/video01.mp4"
+            />
+          </Modal>
+        </div>
+    </div>
+  );
 }
 
 export default Modalver;
